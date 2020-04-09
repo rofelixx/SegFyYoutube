e558f6a948bd8e38f7d425a359ed1c99736b63c9
Rodrigo Felix <rofelix@live.com>
2020-04-09 04:27:40 -0300

    Commit Inicial

---
+++ SegFyYoutube.Tests/UnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SegFyYoutube.Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
